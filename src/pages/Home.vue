e381d337c90941610faf56c848ded53f78e96c92
JinsupJung <cool4uh@gmail.com>
2023-02-26 19:15:25 +0900

    frontend 3rd

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,8 +3,9 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 12" :key="i">
-            <Card/>
+          <div class="col" v-for="(item, idx) in states.items" :key="idx">
+<!--            <Card/>-->
+            {{item}}
           </div>
         </div>
       </div>
@@ -14,11 +15,23 @@
 </template>
 
 <script>
-import Card from "@/components/Card.vue";
+// import Card from "@/components/Card.vue";
+import axios from "axios";
+import {reactive} from "vue";
 
 export default {
   name: "Home",
-  components: {Card}
+  // components: {Card},
+  setup() {
+    const states = reactive({
+      items: []
+    })
+    axios.get("/api/items").then((res)=>{
+      console.log(res);
+      states.items = res.data;
+    })
+    return { states };
+  }
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-
-})
-module.exports= {
+module.exports = {
   devServer: {
     proxy: {
-      '/': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/': ''}
+      '/api': {
+        target: 'http://localhost:8080'
       }
     }
   }
