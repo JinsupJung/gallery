a3ea9301fd6e85b9a38b2300077c3e29d6d4bcfa
JinsupJung <cool4uh@gmail.com>
2023-02-27 13:09:14 +0900

    frontend 로그인폼까지

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <Header/>
-  <Home/>
+  <RouterView/>
   <Footer/>
 </template>
 
 <script>
 import Footer from "@/components/Footer.vue";
 import Header from "@/components/Header.vue";
-import Home from "@/pages/Home.vue";
+// import Home from "@/pages/Home.vue";
 
 export default {
   name: 'App',
   components: {
-    Home,
+    // Home,
     Header,
     Footer,
   }
