bc942934d9202ee1b6931ffabb7ec8d35e0bf747
JinsupJung <cool4uh@gmail.com>
2023-03-03 14:42:27 +0900

    16 기능보완전 까지

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,9 +7,14 @@
             <h4 class="text-white">Contact</h4>
             <ul class="list-unstyled">
               <li><router-link to="/" class="text-white">메인 화면</router-link> </li>
+              <li v-if="$store.state.account.id">
+                <router-link to="/orders" class="text-white" >주문내역</router-link>
+              </li>
+
               <li>
                 <router-link to="/login" class="text-white" v-if="!$store.state.account.id">로그인</router-link>
                 <router-link to="/login" class="text-white" @click="logout()" v-else>로그아웃</router-link>
+
               </li>
             </ul>
           </div>
