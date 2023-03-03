0aaa3c638fcc3fa70493bdf243d31340ef8f5ce1
JinsupJung <cool4uh@gmail.com>
2023-03-03 17:12:40 +0900

    Doen

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -3,19 +3,19 @@
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
-      <input type="email" v-model="state.form.email" class="form-control" id="floatingInput"
+      <input type="email" @keyup.enter="submit()" v-model="state.form.email" class="form-control" id="floatingInput"
              placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
-      <input type="password" v-model="state.form.password" class="form-control" id="floatingPassword"
+      <input type="password" @keyup.enter="submit()"  v-model="state.form.password" class="form-control" id="floatingPassword"
              placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
 
     <div class="checkbox mb-3">
       <label>
-        <input type="checkbox" value="remember-me"> Remember me
+        <input type="checkbox" value="remember-me" @keyup.enter="submit()" > Remember me
       </label>
     </div>
     <button class="w-100 btn btn-lg btn-primary" @click="submit()">Sign in</button>
